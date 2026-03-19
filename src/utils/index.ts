2c62ebb44e2c81d9395418e462ba22bdf15ec825
Dan Musgrave <dan@freewavedigital.com.au>
2026-03-19 06:09:26 +0000

    File changes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export function createPageUrl(pageName: string) {
     return '/' + pageName.replace(/ /g, '-');
 }
+
+export function nanoid(length = 8): string {
+    return Math.random().toString(36).slice(2, 2 + length);
+}
